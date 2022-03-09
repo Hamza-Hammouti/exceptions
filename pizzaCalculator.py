39589affe5b54ea82a72d420bc771805c2bf4a2e
HamzaHammouti <hamzahhammouti@gmail.com>
2022-03-09 09:58:15 +0100

    Exceptions toegevoegd aan PizzaCalculator

---
+++ pizzaCalculator.py
@@ -0,0 +1,25 @@
+# Naam: Hamza Hammouti
+# Opdracht: Pizza Calculator
+
+print("----------------------------------------------------")
+while True:
+    try:
+        aantalSmall = int(input("| Aantal pizza's small (25cm - prijs: €9.49)   : "))
+        aantalMedium = int(input("| Aantal pizza's medium (30cm - prijs: €11.49)   : "))
+        aantalLarge = int(input("| Aantal pizza's large (35cm - prijs: €14.49)    : "))
+        break
+    except ValueError:
+        print("Voer alleen hele getallen in!")
+
+print("----------------------------------------------------")
+
+prijsSmall = 9.49
+prijsMedium = 11.49
+prijsLarge = 14.49
+
+totaalSmall = aantalSmall * prijsSmall
+totaalMedium = aantalMedium * prijsMedium
+totaalLarge = aantalLarge * prijsLarge
+
+totaalprijs = (aantalSmall * prijsSmall) + (aantalMedium * prijsMedium) + (aantalLarge * prijsLarge)
+print (f"Je hebt {aantalSmall} pizza small besteld (€{totaalSmall}), {aantalMedium} pizza medium (€{totaalMedium}) en {aantalLarge} pizza large (€{totaalLarge}). Totaalprijs: €{totaalprijs}")
